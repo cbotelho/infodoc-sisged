be4017c72c2bd78793e896b00cad06055bb9ae0a
GitHub User <your.email@example.com>
2026-01-27 20:32:33 -0300

    usando base localhost

--- config/database.php
+++ config/database.php
@@ -2,8 +2,8 @@
 
 // define database connection
   define('DB_SERVER', 'localhost'); // eg, localhost - should not be empty for productive servers
-  define('DB_SERVER_USERNAME', 'u578749560_botelho_sisged');
-  define('DB_SERVER_PASSWORD', '@#Botelho751953#@');
+  define('DB_SERVER_USERNAME', 'carlos.botelho');
+  define('DB_SERVER_PASSWORD', '@#cvb234#@');
   define('DB_SERVER_PORT', '');		
-  define('DB_DATABASE', 'u578749560_sisged');  	  
+  define('DB_DATABASE', 'infodoc_sisged');  	  
   
